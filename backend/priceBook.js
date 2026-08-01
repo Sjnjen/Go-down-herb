322cc477512272487eb7984ef81f92a8784a97c7
VHUTHU NDOU <164110894+Sjnjen@users.noreply.github.com>
2026-08-01 19:10:34 +0200

    Update priceBook.js

--- backend/priceBook.js
+++ backend/priceBook.js
@@ -2,7 +2,6 @@
 // The frontend also has its own copy (for display), but the backend
 // NEVER trusts prices sent from the browser - it looks them up here.
 // If you change a price on the website, change it here too.
-
 const HERB_PRICES = {
   gdh: 150, // Go Down Herb (Ndaa)
   ugh: 200, // Underground Herb (Ndaa)
@@ -13,9 +12,16 @@ const HERB_PRICES = {
 };
 
 const MERCH_PRICES = {
-  'm-black': 100,
-  'm-white': 100,
-  'm-leaf': 100
+  'm-clans':    100, // Go Down Clans (1 type)
+  'm-black-1':  100, // Go Down Merchandise Black T-Shirt - Design 1 (2 types)
+  'm-black-2':  100, // Go Down Merchandise Black T-Shirt - Design 2 (2 types)
+  'm-white-1':  100, // Go Down Merchandise White T-Shirt - Design 1 (2 types)
+  'm-white-2':  100, // Go Down Merchandise White T-Shirt - Design 2 (2 types)
+  'm-godown':   100, // Go Down T-Shirt (1 type)
+  'm-hands':    100, // Go Down T-Shirt Hands (1 type)
+  'm-ikho':     100, // Ikho Verify (1 type)
+  'm-itsaherb': 100, // It's A Herb T-Shirt (3 types)
+  'm-ndaaweeh': 100  // Ndaa Weeh T-Shirt (3 types)
 };
 
 // Flat delivery fees. "bolt" is intentionally left out - that method
